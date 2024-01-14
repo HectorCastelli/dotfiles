d237d35ddf0a91849072e8875656c81d3edca4f7
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2024-01-14 20:33:24 +0100

    add script for disabling sleep

---
+++ scripts/disable-sleep.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+echo "This will disable sleep on a linux machine."
+echo "This affects the following systemd targets:
+    - sleep.target
+    - suspend.target
+    - hibernate.target
+    - hybrid-sleep.target
+"
+
+sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
