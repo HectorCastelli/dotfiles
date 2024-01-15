e3b408196c0078c489b96e9dd845b02a94a311e1
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2024-01-15 21:08:25 +0100

    add step to check zsh install

--- setup.sh
+++ setup.sh
@@ -1,4 +1,55 @@
-#!/bin/zsh
+#!/bin/bash
+
+install_zsh() {
+    if command -v zsh &>/dev/null; then
+        echo "Zsh is already installed."
+        return 0
+    else
+        # Determine the operating system
+        case "$(uname -s)" in
+            Darwin)
+                install_zsh_macos
+                ;;
+            Linux)
+                # Check if it's Fedora
+                if [ -e /etc/fedora-release ]; then
+                    install_zsh_fedora
+                else
+                    echo "Unsupported Linux distribution. Please install Zsh manually."
+                    exit 1
+                fi
+                ;;
+            *)
+                echo "Unsupported operating system. Please install Zsh manually."
+                exit 1
+                ;;
+        esac
+    fi
+}
+
+install_zsh_macos() {
+    # TODO: Install with nix instead
+    if command -v brew &>/dev/null; then
+        echo "Installing Zsh using Homebrew..."
+        brew install zsh
+    else
+        echo "Homebrew is not installed. Please install Homebrew first."
+        exit 1
+    fi
+}
+
+install_zsh_fedora() {
+    if command -v dnf &>/dev/null; then
+        echo "Installing Zsh using dnf..."
+        sudo dnf install -y zsh
+    else
+        echo "dnf is not installed. Please install dnf first."
+        exit 1
+    fi
+}
+
+echo "Check ZSH installation"
+install_zsh
 
 echo "Fetching dotfiles"
 git clone https://github.com/HectorCastelli/dotfiles "$HOME/dotfiles"
