03300d672cf6e4c35e3a1c0c59f4ad5bf06e67a8
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2024-01-17 16:36:51 +0100

    add tldr to global packages

--- nix/global.sh
+++ nix/global.sh
@@ -1,8 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Setting up global packages"
-nix-env --install --attr nixpkgs.jq
-nix-env --install --attr nixpkgs.yq
-nix-env --install --attr nixpkgs.vscode
-nix-env --install --attr nixpkgs.bat
+
+packages=(
+  "jq"
+  "yq"
+  "vscode"
+  "bat"
+  "tldr"
+)
+
+for package in "${packages[@]}"; do
+    echo "Installing $package"
+    nix-env --install --attr "nixpkgs.$package"
+done
 
