9ae0d678a943bac21ab7ff2bde75da5e0c71d163
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2024-01-26 17:42:17 +0100

    remove all rust stuff

--- installer/src/nix.rs
+++
@@ -1,9 +0,0 @@
-use std::process::Command;
-
-pub(crate) fn install_nix() {
-    println!("Installing nix package manager");
-
-    if Command::new("nix").status().is_ok() {
-        println!("Already installed. Moving on...")
-    }
-}
