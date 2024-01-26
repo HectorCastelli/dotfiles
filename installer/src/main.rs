4154ff63dfbd417a6cd46025621e17aab0b54f98
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2024-01-26 17:22:09 +0100

    setup git clone

--- installer/src/main.rs
+++ installer/src/main.rs
@@ -1,11 +1,41 @@
+use std::path::{Path, PathBuf};
+
+use git2::Repository;
+use inquire::{validator::Validation, Text};
+
 fn main() {
     println!("Starting dotfiles setup");
 
-    todo!("clone repository");
-    todo!("setup symlinks");
+    let repository = close_repository();
+    todo!("setup symlinks to {:?}", repository);
     todo!("setup nix");
     todo!("setup zsh");
     todo!("install global utilities");
     todo!("install GUI apps");
     todo!("setup terminal configuration");
+}
+
+fn close_repository() -> PathBuf {
+    println!("Cloning dotfiles repository");
+
+    let validator = |input: &str| {
+        if Path::new(input).exists() {
+            Ok(Validation::Invalid(
+                "This cannot be an existing directory".into(),
+            ))
+        } else {
+            Ok(Validation::Valid)
+        }
+    };
+
+    let path = Text::new("Where should we save the repository?")
+        .with_default("./dotfiles")
+        .with_validator(validator)
+        .prompt()
+        .expect("An error happened when choosing the clone location.");
+
+    match Repository::clone("https://github.com/HectorCastelli/dotfiles/", &path) {
+        Ok(repo) => Path::new(&path).to_path_buf(),
+        Err(e) => panic!("Failed to clone repository: {}", e),
+    }
 }
