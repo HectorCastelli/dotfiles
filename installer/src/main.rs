32818c9d610d2eaa9829b237e3348b06cff2f245
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2024-01-26 16:52:04 +0100

    add todos to installer

--- installer/src/main.rs
+++ installer/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Starting dotfiles setup");
+
+    todo!("clone repository");
+    todo!("setup symlinks");
+    todo!("setup nix");
+    todo!("setup zsh");
+    todo!("install global utilities");
+    todo!("install GUI apps");
+    todo!("setup terminal configuration");
 }
