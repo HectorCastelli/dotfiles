4465e1462b62ed9967a08b7cfc5c8b78d31326c6
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2024-01-31 21:42:46 +0100

    fix finishing of symlinks

--- setup/installers/home.sh
+++ setup/installers/home.sh
@@ -28,13 +28,7 @@ install_home() {
             ln -sf "$file" "$HOME/$target"
         fi
     done
-    if check_command "$COMMAND"; then
-        success "Command $COMMAND installed correctly"
-        return 0
-    else
-        error "Command $COMMAND was not installed correctly"
-        exit 1
-    fi
+    return 0
 }
 
 install_home_macos() {
