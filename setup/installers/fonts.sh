90425140aac5c800d0c37cd0fa742ec6236306bb
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2024-01-31 22:29:46 +0100

    fix font installation

--- setup/installers/fonts.sh
+++ setup/installers/fonts.sh
@@ -11,13 +11,12 @@ install_fonts() {
 
 install_fonts_monaspace() {
     debug "monaspace"
-    git clone https://github.com/githubnext/monaspace.git
         if is_linux; then
             debug "Initializing user font directory"
             mkdir -p "$HOME/.local/share/fonts"
-            sh -c 'cd "$HOME/dotfiles/fonts/monaspace/" && exec ./util/install_linux.sh'
+            sh -c "cd ./fonts/monaspace/util && chmod +x ./install_linux.sh && exec ./install_linux.sh"
         elif is_macos; then
-            sh -c 'cd "$HOME/dotfiles/fonts/monaspace/" && exec ./util/install_macos.sh'
+            sh -c "cd ./fonts/monaspace/util && chmod +x ./install_macos.sh && exec ./install_macos.sh"
         else
             error "Unsupported operating system"
             exit 1
