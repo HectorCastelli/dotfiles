dedcdea551f57147c9b229b5f5b999403db8f989
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2024-01-31 17:04:02 +0100

    add back nix version for linux

--- setup/espanso.sh
+++ setup/espanso.sh
@@ -11,10 +11,30 @@ if command -v espanso >/dev/null 2>&1; then
 else
     case "$(uname -s)" in
     Linux)
-        nix-env --install --attr nixpkgs.espanso
+        session_type=$XDG_SESSION_TYPE
+
+        case "$session_type" in
+        x11)
+            nix-env --install --attr nixpkgs.espanso
+            ;;
+        wayland)
+            warn "Wayland support is experimental and not very reliable"
+            nix-env --install --attr nixpkgs.espanso-wayland
+            ;;
+        *)
+            echo "Unsupported session type: $session_type. Exiting."
+            exit 1
+            ;;
+        esac
+
         info "Starting up espanso service"
         espanso service register
         espanso start
+
+        if [ "$session_type" = "wayland" ]; then
+            warn "Adding permissions to monitor inputs"
+            sudo setcap "cap_dac_override+p" "$(readlink "$(which espanso)")"
+        fi
         ;;
     *)
         error "Unsupported operating system. Please install manually."
