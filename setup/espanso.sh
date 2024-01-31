68a73321ddc1e4d49c418fe9c52a770b6ad21954
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2024-01-31 16:00:07 +0100

    use global install for espanso

--- setup/espanso.sh
+++ setup/espanso.sh
@@ -9,34 +9,13 @@ if command -v espanso >/dev/null 2>&1; then
     success "espanso is already installed."
     exit 0
 else
-    case "$(uname -s)" in
-    Linux)
-        _session="$XDG_SESSION_TYPE"
-        case "$_session" in
-        wayland)
-            nix-env --install --attr nixpkgs.espanso-wayland
-            ;;
-        x11)
-            nix-env --install --attr nixpkgs.espanso
-            ;;
-        *)
-            error "Unsupported session type: $session_type. Please install manually."
-            open "https://espanso.org/install/"
-            warn "Press ENTER once installed and the setup will continue."
-            read -r _dummy
-            exit 1
-            ;;
-        esac
+    open "https://espanso.org/install/"
+    warn "Press ENTER once installed and the setup will continue."
+    read -r _dummy
+
+    if [ "$(uname -s)" = "Linux" ]; then
         info "Starting up espanso service"
         espanso service register
         espanso start
-        ;;
-    *)
-        error "Unsupported operating system. Please install manually."
-        open "https://espanso.org/install/"
-        warn "Press ENTER once installed and the setup will continue."
-        read -r _dummy
-        exit 1
-        ;;
-    esac
+    fi
 fi
