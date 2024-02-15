960ff01d149877b62d4d9f092b9c75c06dcba5ab
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2024-02-15 13:58:16 +0100

    fix git clean to clean all deleted remotes

--- shell/aliases/git.sh
+++ shell/aliases/git.sh
@@ -10,7 +10,12 @@ alias gc='git commit --verbose'
 alias gca='git commit --verbose --amend'
 alias gcm='git commit --verbose --message'
 alias gcnm='git commit --verbose --no-verify --message'
-alias gcl='git branch --merged | egrep -v "(^\*|master|main|dev)" | xargs git branch -d'
+gcl() {
+    info "Deleting local branches that no longer have a remote"
+    git branch -vv | grep 'gone]' | awk '{print $1}' | xargs git branch -D
+    info "Listing local branches that do not have a remote"
+    git branch --no-merged | awk '{print $1}'
+}
 alias gf='git fetch'
 alias glo='git log --oneline --decorate'
 alias glo!='git log --graph'
