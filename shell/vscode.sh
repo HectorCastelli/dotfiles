9082616e5e5d06b635b2d7b5a7807a2f02950cb4
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2024-03-22 12:39:24 +0100

    add automatic vscode setup for plugins and proxy

--- shell/vscode.sh
+++ shell/vscode.sh
@@ -2,3 +2,9 @@
 
 export EDITOR="code --wait"
 export EDITOR_NON_BLOCKING="code --new-window"
+
+# Automatically handle specific use-cases for vscode integrated terminal
+if [ "$TERM_PROGRAM" = "vscode" ]; then
+    export ALMOSONTOP='false'
+    proxy_autoload
+fi
