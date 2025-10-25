661a650fcd37059604c3ff6e1844bafda10d84f9
HectorCastelli <hector.zacharias@gmail.com>
2025-10-25 20:43:57 +0200

    feat(profiles): create profile template

---
+++ profiles/_template/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+set -u
+
+# This script is the base for installing a new profile.
+# It is intended to be copied and modified for each new profile.
+# It must be idempotent and handle re-execution gracefully.
---
+++ profiles/_template/uninstall.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -u
+
+# This script is the base for uninstalling a new profile.
+# It is intended to be copied and modified for each new profile.
+# When removing something, be cautious to only delete those that were created by the install script.
