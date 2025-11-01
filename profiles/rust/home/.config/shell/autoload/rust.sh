ebd3f5c4683a64ff366e54a7152564eee0c90829
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2025-11-01 23:21:37 +0100

    fix(rust): make rust load into the env

---
+++ profiles/rust/home/.config/shell/autoload/rust.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Add Rust to the environment
+. "$HOME/.cargo/env"
--- profiles/rust/install.sh
+++ profiles/rust/install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 set -eu
 
+DOTFILES_DIR=${DOTFILES_DIR:-"$HOME/dotfiles"}
+TARGET_DIR="${TARGET_DIR:-$DOTFILES_DIR/.target}"
+
 # Install gcc and make via homebrew
 brew install gcc
 brew install make
@@ -11,6 +14,5 @@ brew install rustup
 # Initialize rustup and install the default stable toolchain
 rustup-init -y --default-toolchain stable
 
-# Verify installation
-rustc --version
-cargo --version
+# Source cargo environment
+. "$HOME/.cargo/env"
