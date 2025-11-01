cdc8cf58394b0a1d34ef9c35854844febe9afd96
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2025-11-01 16:23:52 +0100

    fix(dev_node): fnm is setup in a temporary zsh shell

--- profiles/dev_node/install.sh
+++ profiles/dev_node/install.sh
@@ -11,10 +11,7 @@ mkdir -p "$TARGET_DIR/home/.config/shell/autoload"
 fnm env --use-on-cd --shell=zsh >"$TARGET_DIR/home/.config/shell/autoload/fnm.sh"
 chmod +x "$TARGET_DIR/home/.config/shell/autoload/fnm.sh"
 
-# Load fnm in the current shell session
-# shellcheck source=/dev/null
-. "$TARGET_DIR/home/.config/shell/autoload/fnm.sh"
-
 # Install and use latest LTS version of Node.js
-fnm install --lts
-fnm use lts-latest
+zsh -c "eval \"\$(fnm env --use-on-cd --shell=zsh)\" && \
+    fnm install --lts && \
+    fnm use lts-latest"
