27c4f1ad9bd169c44c90fa6d5541634b4a44c8d9
HectorCastelli <hector.zacharias@gmail.com>
2025-11-01 11:58:05 +0100

    feat: add github to dev profile

--- profiles/dev/install.sh
+++ profiles/dev/install.sh
@@ -15,3 +15,13 @@ else
 	echo "Error: unsupported operating system" >&2
 	exit 1
 fi
+
+brew install gh
+
+if ! gh auth status --hostname github.com >/dev/null 2>&1; then
+	gh auth login --hostname github.com --scopes admin:ssh_signing_key
+	gh auth refresh --hostname github.com --scopes admin:public_key
+fi
+
+gh ssh-key add "$TARGET_DIR/home/.ssh/id_ed25519.pub" --title "$(hostname) authentication" --type authentication
+gh ssh-key add "$TARGET_DIR/home/.ssh/id_ed25519_signing.pub" --title "$(hostname) signing" --type signing
--- profiles/dev/uninstall.sh
+++ profiles/dev/uninstall.sh
@@ -7,8 +7,9 @@ elif [ "$(uname)" = "Darwin" ]; then
 	brew uninstall --cask bruno 2>/dev/null || true
 fi
 
-brew uninstall lnav 2>/dev/null || true
-brew uninstall lcov 2>/dev/null || true
-brew uninstall graphviz 2>/dev/null || true
-brew uninstall shellcheck 2>/dev/null || true
+brew uninstall gh 2>/dev/null || true
 brew uninstall shfmt 2>/dev/null || true
+brew uninstall shellcheck 2>/dev/null || true
+brew uninstall graphviz 2>/dev/null || true
+brew uninstall lcov 2>/dev/null || true
+brew uninstall lnav 2>/dev/null || true
