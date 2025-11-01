7b711317c77c87b2f7390369fcbd5aad9359c2f9
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2025-11-01 23:18:51 +0100

    fix(gnome): correct app installation

--- profiles/gnome/install.sh
+++ profiles/gnome/install.sh
@@ -3,9 +3,10 @@ set -eu
 
 if [ "$(uname)" = "Linux" ]; then
 	flatpak install -y flathub io.github.flattool.Warehouse
-	flatpak install -y flathub org.gnome.baobab
-	flatpak install -y flathub org.gnome.tweaks
+	flatpak install -y flathub io.github.kolunmi.Bazaar
+	flatpak install -y flathub page.tesk.Refine
 	flatpak install -y flathub com.mattjakeman.ExtensionManager
+	flatpak install -y flathub io.github.realmazharhussain.GdmSettings
 elif [ "$(uname)" = "Darwin" ]; then
 	echo "GNOME does not run on macOS. Skipping installation."
 fi
--- profiles/gnome/uninstall.sh
+++ profiles/gnome/uninstall.sh
@@ -3,9 +3,10 @@ set -eu
 
 if [ "$(uname)" = "Linux" ]; then
 	flatpak uninstall -y flathub io.github.flattool.Warehouse 2>/dev/null || true
-	flatpak uninstall -y flathub org.gnome.baobab 2>/dev/null || true
-	flatpak uninstall -y flathub org.gnome.tweaks 2>/dev/null || true
+	flatpak uninstall -y io.github.kolunmi.Bazaar 2>/dev/null || true
+	flatpak uninstall -y flathub page.tesk.Refine 2>/dev/null || true
 	flatpak uninstall -y flathub com.mattjakeman.ExtensionManager 2>/dev/null || true
+	flatpak uninstall -y flathub io.github.realmazharhussain.GdmSettings 2>/dev/null || true
 elif [ "$(uname)" = "Darwin" ]; then
 	# Nothing to do on macOS
 	:
