45844c64f146d536e914493b2ea888dda55fe538
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2025-11-01 16:29:59 +0100

    fix: vscode is isntalled

--- profiles/0/install.sh
+++ profiles/0/install.sh
@@ -86,8 +86,8 @@ brew install bitwarden-cli
 if [ "$(uname)" = "Linux" ]; then
 	sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 	echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo >/dev/null
-	# dnf check-update
-	# sudo dnf install --assumeyes code
+	dnf check-update
+	sudo dnf install -y code
 else
 	brew install --cask visual-studio-code
 fi
