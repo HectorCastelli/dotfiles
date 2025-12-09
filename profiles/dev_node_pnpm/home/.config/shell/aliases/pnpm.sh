a56e2d3be5bc1443bbe2619fcf42b963249ff32e
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2025-12-09 21:06:06 +0100

    feat(dev_node_pnpm): add profile for pnpm

---
+++ profiles/dev_node_pnpm/home/.config/shell/aliases/pnpm.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+alias pn='pnpm'
+alias pni='pnpm install'
+alias pnid='pnpm install --save-dev'
+alias pnr='pnpm run'
+alias pnt='pnpm test'
+alias pntci='pnpm run test:ci'
+alias pnx='pnpm dlx'
---
+++ profiles/dev_node_pnpm/uninstall.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+set -eu
+brew uninstall pnpm 2>/dev/null || true
